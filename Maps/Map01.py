76bd37b459acd53bcbf6f800240a4c0d197008fd
Guilherme <guilherme.cardoso@valecard.com.br>
2020-05-18 17:25:22 -0300

    Mapeado o COrredor

--- Maps/Map01.py
+++ Maps/Map01.py
@@ -1,9 +1,10 @@
 from Maps.Map import Map
 from Tiles.Teatro import Teatro
 from Tiles.Lobby import Lobby
+from Tiles.Corredor import Corredor
 class Map01(Map):
 	"""docstring for Map01"""
 	def __init__(self):
-		self.tiles = [Teatro(), Lobby()]
+		self.tiles = [Teatro(), Lobby(), Corredor()]
 		self.max_tiles = 4		
 		# super(Map01, self).__init__(lista_de_tiles, max_tiles)
---
+++ Tiles/Corredor.py
@@ -0,0 +1,19 @@
+from Tiles.Tile import Tile
+class Corredor(Tile):
+	"""docstring for Corredor"""
+	def __init__(self):
+		self.nome = 'Corredor';
+		self.largura = 2;
+		self.altura = 1;
+		self.num_portas = 4;
+		self.e_comeco = True;
+		self.path_image = 'mansion_img/corredor.jpeg';
+
+
+
+
+
+
+
+
+		
